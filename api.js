33fcd3b013d536ebf4204e996d72ec699a34dbe4
Jacquiiii <jacqui.koroll@telus.com>
2023-01-02 20:32:17 -0800

    combined all helper functions on api.js

--- api.js
+++ api.js
@@ -4,18 +4,24 @@ require('dotenv').config();
 // install axios in project: npm i axios
 const axios = require("axios");
 
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
 // API keys from .env files
 const bookKey = process.env.BOOK_KEY;
 const movieKey = process.env.MOVIE_KEY;
+const restaurantKey = process.env.RESTAURANT_KEY;
+
+// Yelp code. Not working.
+// const yelp = require('yelp-fusion');
+// const client = yelp.client(restaurantKey);
 
 
 // Google Books API: https://developers.google.com/books/docs/v1/using
 const getBooks = (text) => {
 
   return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}&key=${bookKey}`)
-    .then((response) => {
-      return response.data.items;
-    })
+    .then(response => response.data.items)
     .catch((error) => console.error(error.message));
 
 };
@@ -35,13 +41,33 @@ const getMovies = (text) => {
   };
 
   return axios.request(options)
-    .then((response) => {
+    .then(response => {
       const movieDetails = response.data.d;
       return movieDetails[0];
     })
     .catch((error) => console.log(error.message));
 
 };
+
+
+// Yelp API client for restaurants: https://github.com/tonybadguy/yelp-fusion
+// not working
+'use strict';
+const getRestaurants = (text) => {
+
+  client.search({
+    term: text,
+    location: 'canada',
+  }).then(response => {
+    console.log(response.jsonBody.businesses);
+    console.log(response.jsonBody.businesses[0].name);
+  }).catch(e => {
+    console.log(e);
+  });
+
+};
+
+getRestaurants('how to lose a guy in ten days');
 
 
 // Finds category based on predefined text. If no positive result is found, getCategory can be used to call APIs.
@@ -57,31 +83,74 @@ const categoryCheck1 = (text) => {
 }
 
 
-// Calls APIs until category is found
+// Calls APIs until category is found. If no category is found, returns null.
 const categoryCheck2 = (text) => {
 
   return new Promise((resolve, reject) => {
     getMovies(text)
       .then(response => {
-        if (response.qid === 'movie') {
-          console.log('Movies');
-          resolve('Movies');
-        } else {
-          getBooks(text)
-          .then(response => {
-            if (response.length > 0) {
-              console.log('Books');
-              resolve('Books');
-            }
-          })
-          .catch((error) => reject(error.message));
-        }
+        if (response.qid === 'movie') resolve('Movies');
+        return;
       })
       .catch((error) => reject(error.message));
-  })
+
+    getBooks(text)
+      .then(response => {
+        if (response.length > 0) resolve('Books');
+        return;
+      })
+      .catch((error) => reject(error.message));
+
+    // not working:
+    // getRestaurants(text)
+    //   .then(response => {
+    //     if (response.businesses.length > 0) resolve('Restaurants');
+    //     return;
+    //   })
+    //   .catch((error) => reject(error.message));
+
+    });
 
 };
 
 
-module.exports = { categoryCheck1, categoryCheck2 };
+// ------------ Google natural language API function ------------ //
 
+
+// Returns a promise. Requires npm install --save @google-cloud/language.
+async function quickstart(text) {
+
+  // Creates a client
+  const client = new language.LanguageServiceClient();
+  // Prepares a document, representing the provided text
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+  // V2 for Google Natural Language
+  const classificationModelOptions = { v2Model: { contentCategoriesVersion: 'V2'} };
+  // Classifies text in the document
+  const [classification] = await client.classifyText({
+    document,
+    classificationModelOptions
+  });
+  // Updated to true if the text matches a certain category
+  let restaurantWinner, movieWinner, bookWinner, productWinner = false;
+
+  // Loops through categories and updates variables to true if category is found
+  for (category of classification.categories) {
+    if (category.name.includes('Restaurant')) restaurantWinner = true;
+    if (category.name.includes('Movie')) movieWinner = true;
+    if (category.name.includes('Book')) bookWinner = true;
+    if (category.name.includes('Shopping') || category.name.includes('Games') || category.name.includes('Home & Garden') || category.name.includes('Computers & Electronics')) productWinner = true;
+  }
+
+  // Returns category matching the text
+  if (restaurantWinner) return 'Restaurants';
+  if (movieWinner) return 'Movies';
+  if (bookWinner) return 'Books';
+  if (productWinner) return 'Products';
+
+}
+
+module.exports = { quickstart, categoryCheck1, categoryCheck2 };
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const { categoryCheck1, categoryCheck2 } = require('./api.js');
+const { categoryCheck1, categoryCheck2, quickstart } = require('./api.js');
 
 
 // -------------------- Sass code -------------------- //
@@ -47,7 +47,6 @@ app.get('/', (req, res) => {
 });
 
 
-// takes in text from
 app.post('/tasks', function(req, res) {
 
   // synchronous check without api checks if input includes certain words matching specific categories and adds to database if one matches
@@ -87,6 +86,46 @@ app.post('/tasks', function(req, res) {
     }
 
 });
+
+
+// works with post route on public/scripts/app.js to insert form input into database after categorizing the data
+// app.post('/tasks', function(req, res) {
+
+    // synchronous check without api checks if input includes certain words matching specific categories and adds to database if one matches
+//   const firstCheck = categoryCheck1(req.body.name);
+//   if (firstCheck) {
+//     const category = firstCheck;
+//     const values = [req.body.name, category];
+//     const queryString = `
+//       INSERT INTO tasks (name, category)
+//       VALUES ($1, $2)
+//       RETURNING *;
+//       `;
+
+//     db.query(queryString, values)
+//       .then(() => res.send('Success'))
+//       .catch(() => res.send(err));
+
+    // asynch check calls Google Natural Language API and checks input against response data to determine if it could fall into one of those categories
+//   } else {
+//     quickstart(req.body.name)
+//     .then((result) => {
+//       const category = result;
+//       const values = [req.body.name, category];
+//       const queryString = `
+//         INSERT INTO tasks (name, category)
+//         VALUES ($1, $2)
+//         RETURNING *;
+//         `;
+//       db.query(queryString, values)
+//         .then(() => res.send('Success'))
+//         .catch(() => res.send(err));
+//       })
+//     .catch((err) => console.log(err));
+//   }
+
+// });
+
 
 // alternative to the above without api to categorize
 // app.post('/tasks', function(req, res) {
